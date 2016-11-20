0721f1fe481fa8936d1bb6357160c9acd1996f0e
Igor Khrupin <igor@hrupin.com>
2016-11-20 14:02:31 +0100

    added qsort algorithm

---
+++ src/com/hrupin/algorithms/Main.java
@@ -0,0 +1,11 @@
+package com.hrupin.algorithms;
+
+import com.hrupin.algorithms.sort.QuickSortSample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		QuickSortSample.run();
+	}
+
+}
