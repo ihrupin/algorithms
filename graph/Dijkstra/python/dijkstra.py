1a17505a8541ddbfa7735251b9dff4063f3f3846
Igor Khrupin <igor@hrupin.com>
2020-12-16 22:59:50 -0800

    changed common methods

--- graph/Dijkstra/python/dijkstra.py
+++ graph/Dijkstra/python/dijkstra.py
@@ -1,4 +1,30 @@
 import graphMethods
 
-graphMethods.initGraph()
-graphMethods.printGraph()
+def initGraph():
+    graph = {}
+
+    graphMethods.addVertexToDictionary(graph, 1)
+    graphMethods.addVertexToDictionary(graph, 2)
+    graphMethods.addVertexToDictionary(graph, 3)
+    graphMethods.addVertexToDictionary(graph, 4)
+    graphMethods.addVertexToDictionary(graph, 5)
+    graphMethods.addVertexToDictionary(graph, 6)
+
+    graphMethods.addEdge(graph, 1, 2, 6)
+    graphMethods.addEdge(graph, 1, 3, 4)
+
+    graphMethods.addEdge(graph, 2, 4, 7)
+    graphMethods.addEdge(graph, 2, 5, 4)
+    graphMethods.addEdge(graph, 2, 6, 2)
+
+    graphMethods.addEdge(graph, 3, 4, 3)
+    graphMethods.addEdge(graph, 3, 5, 8)
+
+    graphMethods.addEdge(graph, 4, 6, 10)
+
+    graphMethods.addEdge(graph, 5, 6, 1)
+
+    return graph
+
+graph = initGraph()
+graphMethods.printGraph(graph)
--- graph/Dijkstra/python/graphMethods.py
+++ graph/Dijkstra/python/graphMethods.py
@@ -1,14 +1,10 @@
-def addVertexToDictionary(vertex):
-  global graph
-  global graphVerticesCount
+def addVertexToDictionary(graph, vertex):
   if vertex in graph:
     print("Vertex ", vertex, " already exists.")
   else:
-    graphVerticesCount += 1
     graph[vertex] = []
 
-def addEdge(vertex1, vertex2, edgeEeight):
-  global graph
+def addEdge(graph, vertex1, vertex2, edgeEeight):
   if vertex1 not in graph:
     print("Vertex ", vertex1, " does not exist.")
   elif vertex2 not in graph:
@@ -20,33 +16,7 @@ def addEdge(vertex1, vertex2, edgeEeight):
     temp = [vertex2, edgeEeight]
     graph[vertex1].append(temp)
 
-def printGraph():
-  global graph
+def printGraph(graph):
   for vertex in graph:
     for edges in graph[vertex]:
       print(vertex, " -> ", edges[0], " edge weight: ", edges[1])
-
-graph = {}
-graphVerticesCount = 0
-
-def initGraph():
-    addVertexToDictionary(1)
-    addVertexToDictionary(2)
-    addVertexToDictionary(3)
-    addVertexToDictionary(4)
-    addVertexToDictionary(5)
-    addVertexToDictionary(6)
-
-    addEdge(1, 2, 6)
-    addEdge(1, 3, 4)
-
-    addEdge(2, 4, 7)
-    addEdge(2, 5, 4)
-    addEdge(2, 6, 2)
-
-    addEdge(3, 4, 3)
-    addEdge(3, 5, 8)
-
-    addEdge(4, 6, 10)
-
-    addEdge(5, 6, 1)
