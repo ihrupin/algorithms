f9a3cfc235e82f2d5b8c984f4fb189471854c3f1
Igor Khrupin <igor@hrupin.com>
2020-12-26 23:26:29 -0800

    added Reverse Linked List algotithm in Python

--- linkedList/Reverse_a_linked_list/python/reverseLinkedList.py
+++ linkedList/Reverse_a_linked_list/python/reverseLinkedList.py
@@ -1,11 +1,49 @@
 class Node:
-    value = 0
-    nextElement = None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-    def __init__(self, value, nextElement):
-        self.value = value 
-        self.nextElement = nextElement
 
-node = Node(100, None)
+class LinkedList:
+    def __init__(self):
+        self.head = None
+ 
+    def reverse(self):
+        prev = None
+        current = self.head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+ 
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+    def printList(self):
+        temp = self.head
+        stringBuffer = ""
+        while(temp):
+            stringBuffer += str(temp.data)
+            temp = temp.next
+            if (temp):
+                stringBuffer += "--->"
+            else:
+                stringBuffer += "--->NULL"
+        print(stringBuffer)
 
-print(node.value)
+
+llist = LinkedList()
+llist.push(13)
+llist.push(5)
+llist.push(98)
+llist.push(8)
+ 
+print("Given Linked List")
+llist.printList()
+llist.reverse()
+print("Reversed Linked List")
+llist.printList()
