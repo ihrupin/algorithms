9801bf9adc7055a81decc83191662eec2b54d7f5
Igor Khrupin <igor@hrupin.com>
2020-12-28 00:20:51 -0800

    added Reverse Linked List in groups K algorithm in Python

--- linkedList/Reverse_a_linked_list_in_groups_of_a_given_size/python/reverseLinkedListInGroups.py
+++ linkedList/Reverse_a_linked_list_in_groups_of_a_given_size/python/reverseLinkedListInGroups.py
@@ -45,14 +45,15 @@ class LinkedList:
 
 
 llist = LinkedList()
-llist.push(13)
-llist.push(5)
-llist.push(98)
-llist.push(8)
 llist.push(12)
-llist.push(89)
-llist.push(24)
-llist.push(1)
+llist.push(23)
+llist.push(53)
+llist.push(90)
+llist.push(12)
+llist.push(56)
+llist.push(13)
+llist.push(8)
+llist.push(5)
  
 print("Given Linked List")
 llist.printList()
