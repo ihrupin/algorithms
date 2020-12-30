25610d1a910a02b735482810aae5120c55ae66e2
Igor Khrupin <igor@hrupin.com>
2020-12-30 11:42:52 -0800

    added Binary Search recursive algorithm in Python

--- Searching/Binary_search/python/binarySearch.py
+++ Searching/Binary_search/python/binarySearch.py
@@ -1,4 +1,4 @@
-def search(sortedArray, element):
+def binarySearchLoopImplementation(sortedArray, element):
     startIndex = 0
     finishIndex = len(sortedArray) - 1
 
@@ -15,6 +15,20 @@ def search(sortedArray, element):
         elif sortedArray[middleIndex] < element:
             startIndex = middleIndex + 1
 
+def binarySearchRecursively(sortedArray, element, startIndex, finishIndex):
+    middleIndex = int((finishIndex + startIndex) / 2)
+    if sortedArray[middleIndex] == element:
+        return middleIndex
+    elif sortedArray[startIndex] == element:
+        return startIndex
+    elif sortedArray[finishIndex] == element:
+        return finishIndex
+    elif sortedArray[middleIndex] > element:
+        return binarySearchRecursively(sortedArray, element, startIndex, middleIndex - 1)
+    elif sortedArray[middleIndex] < element:
+        return binarySearchRecursively(sortedArray, element, middleIndex + 1, finishIndex)
+
 array = [1, 4, 10, 43, 45, 46, 51, 53, 60, 64, 65, 66, 67, 72, 81, 82, 84, 90, 100]
 
-print("Result:", search(array, 60))
+print("Result for recursively implementation:", binarySearchRecursively(array, 4, 0, len(array) - 1))
+print("Result for loop implementation:", binarySearchLoopImplementation(array, 4))
